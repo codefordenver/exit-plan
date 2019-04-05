863c533dd1a3d6c1a59116b07509ee592eb19330
Derek Hanciles <drhanciles@icloud.com>
2019-04-04 18:24:23 -0600

    Updated Nav component to now havea parent nav html element

---
+++ frontend/src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from '../Nav/Nav.js';
+
+export const Header = () => {
+  return (
+    <header>
+      
+    </header>
+  )
+}
--- frontend/src/Nav/Nav.js
+++ frontend/src/Nav/Nav.js
@@ -1,12 +1,14 @@
-import React from 'react'; 
+import React from 'react';
 
-const navFilters = ['HOME', 'ABOUT', 'RESOURCES', 'ARTICLES', 'CONTACT']
+const navFilters = ['HOME', 'ABOUT', 'RESOURCES', 'ARTICLES', 'CONTACT'];
 
 export const Nav = () => {
   const filterList = navFilters.map(filter => (<li>filter</li>))
   return(
-    <ul>
-      {filterList}
-    </ul>
+    <nav>
+      <ul>
+        { filterList }
+      </ul>
+    </nav>
   )
 }
