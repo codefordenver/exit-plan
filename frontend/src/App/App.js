70d887c7fb9bf9583ed13d0276ed7fed07fb1b58
Derek Hanciles <drhanciles@icloud.com>
2019-04-04 18:37:25 -0600

    Updated the structure of the App component

---
+++ frontend/src/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header.js';
+
+export const App = () => {
+  return (
+    <main className="app-wrapper">
+      <Header />
+    </main>
+  )
+};
--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -4,10 +4,10 @@ import Nav from '../Nav/Nav.js';
 export const Header = () => {
   return (
     <header>
-      <div className="logo-container" role="logo-container">
+      <div className="logo-container" role="logo container">
         <img src="public/images/mock-logo.png" alt="Exit Plan Logo"/>
       </div>
       <Nav/>
     </header>
   )
-}
+};
