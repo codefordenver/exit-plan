2f0dc535c7d76fe23988fd99248fc12d66ced6ac
Derek Hanciles <drhanciles@icloud.com>
2019-04-04 18:43:01 -0600

    Updated map method on line 6 to properly render filters

--- frontend/src/Nav/Nav.js
+++ frontend/src/Nav/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 const navFilters = ['HOME', 'ABOUT', 'RESOURCES', 'ARTICLES', 'CONTACT'];
 
 const Nav = () => {
-  const filterList = navFilters.map(filter => (<li>filter</li>))
+  const filterList = navFilters.map(filter => (<li>{filter}</li>))
   return(
     <nav>
       <ul>
