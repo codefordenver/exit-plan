1d0d689d9d2ccd1c2ab9011cc2d3817be1e02d68
alwayzup <hermanjason@live.com>
2019-04-18 12:07:56 -0600

    Added main and footer components

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.scss';
 import Header from '../Header/Header.js';
+import Main from '../Main/Main.js';
+import Footer from '../Footer/Footer.js';
+
 
 const App = () => {
   return (
     <main className="app-wrapper">
       <Header />
+      <Main />
+      <Footer />
     </main>
   )
 };
