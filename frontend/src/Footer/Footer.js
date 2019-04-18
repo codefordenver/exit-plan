1e2be6435c9c2c14fd3cd93fa071af2f8ab26067
jwexitplan <jwexitplan@protonmail.com>
2019-04-18 13:00:03 -0600

    Added some footer content - still needs formatting

--- frontend/src/Footer/Footer.js
+++ frontend/src/Footer/Footer.js
@@ -4,7 +4,17 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer>
-
+      <div className="footer-location">
+        <h3>Exit Plan</h3>
+        <p>123 W 12th Street,</p>
+        <p>Denver, CO 12345</p>
+      </div>
+      <div className="footer-links">
+        <h3>Useful Links</h3>
+        <a>Customer Support</a>
+        <a>Emergency Resources</a>
+        <a>Directory</a>
+      </div>
     </footer>
   )
 };
