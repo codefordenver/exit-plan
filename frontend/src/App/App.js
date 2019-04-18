ec081f3bb2774f48414cfc756ec9b2b8c65475c4
Derek Hanciles <drhanciles@icloud.com>
2019-04-18 16:37:11 -0600

    Implemented initial styling for GeneralInfo component

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from '../Header/Header.js';
+import GeneralInfo from '../GeneralInfo/GeneralInfo.js'; 
 
 const App = () => {
   return (
     <main className="app-wrapper">
       <Header />
+      <GeneralInfo />
     </main>
   )
 };
--- frontend/src/GeneralInfo/GeneralInfo.js
+++ frontend/src/GeneralInfo/GeneralInfo.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import './GeneralInfo.scss'; 
 
 const GeneralInfo = () => {
   return (
     <section className="general-info-container">
-      <article className="main-logo-container">
-        <img className="primary-logo" src="./assets/Exit_Plan-Logo.svg" alt="primary exit plan logo"/>
-      </article>
-      <article className="left-container">
-        <h3 className="left-header">WHAT IS EXIT PLAN</h3>
-        <p className="lead-text">
+      <img className="primary-logo" src="./assets/Exit_Plan-Logo.svg" alt="primary exit plan logo"/>
+      <div role="secondary container for general info" className="text-video-container">
+          <article className="left-container">
+          <h1>WHAT IS EXIT PLAN?</h1>
+          <p className="lead-text">
           Coming out in a hostile environment is hard. When a member of a family comes out as LBGT or a member of a religious cult decides they want to leave, they can en up feeling like they've lost everything -- their family, their friends, their support structures, and financial support. 
-        </p>
-        <p className="secondary-text">
+          </p>
+          <p className="secondary-text">
           With chat articles and resources Exit Plan is here to help!
-        </p>
-      </article>
-      <article className="right-container">
-      </article>
+          </p>
+        </article>
+        <article className="right-container">
+        </article>
+      </div>
     </section>
   ); 
 }
